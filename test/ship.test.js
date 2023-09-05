4d92d119aa39651a2c2cc86799f595b19b73025b
Karol <kpulawski13@gmail.com>
2023-09-05 18:35:30 +0100

    ship tests

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,6 +1,23 @@
 const Ship = require('../src/ship.js');
 
+let ship;
+
+beforeEach(() => {
+  ship = Ship(3);
+});
 test('creates a ship factoryFunc with specified length', () => {
-  const ship = Ship(8);
-  expect(ship.length).toBe(8);
+  expect(ship.length).toBe(3);
+});
+
+test('hit function increase number of hits on call', () => {
+  const hits = ship.getHits();
+  ship.hit();
+  expect(ship.getHits()).toBe(hits + 1);
+});
+
+test('isSunk function === true if length and hits are equal', () => {
+  const ship = Ship(1);
+  expect(ship.isSunk()).toBe(false);
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
 });
