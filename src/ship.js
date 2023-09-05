8257e5c4749586010c55b1f3a82e8928f909094d
Karol <kpulawski13@gmail.com>
2023-09-05 18:35:08 +0100

    implemented basic functions to ship factory

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,13 @@
 const Ship = (length) => {
-  return { length };
+  let hits = 0;
+
+  const hit = () => (hits += 1);
+
+  const getHits = () => hits;
+
+  const isSunk = () => (getHits() === length ? true : false);
+
+  return { length, hit, getHits, isSunk };
 };
 
 module.exports = Ship;
