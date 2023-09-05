cb53fbf8e3b89a71bf2abe28f4799222b3f4fa1e
Karol <kpulawski13@gmail.com>
2023-09-05 14:01:50 +0100

    add jest to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -28,4 +28,12 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    alias: {
+      // Map asset and style file extensions to the mock files
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        path.resolve(__dirname, '__mocks__/fileMock.js'),
+      '\\.(css|less)$': path.resolve(__dirname, '__mocks__/styleMock.js'),
+    },
+  },
 };
