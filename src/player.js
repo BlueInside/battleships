468c529f2cd4c9c473dfbcfad5915048e55a96f6
Karol <kpulawski13@gmail.com>
2023-09-07 18:19:55 +0100

    implement player shoot function

--- src/player.js
+++ src/player.js
@@ -1,5 +1,12 @@
+const Gameboard = require('../src/gameboard.js');
 const Player = () => {
-  return {};
+  let gameboard = Gameboard();
+
+  const shoot = (cordX, cordY, enemyPlayer) => {
+    enemyPlayer.receiveAttack(cordX, cordY);
+  };
+  const { placeShip, getHitRecords, receiveAttack } = gameboard;
+  return { placeShip, getHitRecords, receiveAttack, shoot };
 };
 
 module.exports = Player;
