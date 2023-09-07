6134ce066b0ed85900fae20f9c1d98e923f49735
Karol <kpulawski13@gmail.com>
2023-09-07 13:59:14 +0100

    Added isShip property

--- src/ship.js
+++ src/ship.js
@@ -1,13 +1,15 @@
 const Ship = (length) => {
   let hits = 0;
 
+  const isShip = true;
+
   const hit = () => (hits += 1);
 
   const getHits = () => hits;
 
   const isSunk = () => (getHits() === length ? true : false);
 
-  return { hit, getHits, isSunk };
+  return { hit, getHits, isSunk, isShip };
 };
 
 module.exports = Ship;
