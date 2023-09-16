001d7a62d5e1d87529eaed9f33caa8c4434bb775
Karol <kpulawski13@gmail.com>
2023-09-16 09:53:16 +0100

    fix size of quess from 8x8 to 10x10 board

--- src/player.js
+++ src/player.js
@@ -21,8 +21,8 @@ const Player = () => {
   // Generates a random move for the CPU player and checks if it's a legal move.
   // If not, recursively calls `cpuMove` until a legal move is found.
   const cpuMove = (enemy) => {
-    let cordX = Math.floor(Math.random() * 8);
-    let cordY = Math.floor(Math.random() * 8);
+    let cordX = Math.floor(Math.random() * 10);
+    let cordY = Math.floor(Math.random() * 10);
     if (isLegal(cordX, cordY, enemy)) shoot(cordX, cordY, enemy);
     else cpuMove(enemy);
   };
